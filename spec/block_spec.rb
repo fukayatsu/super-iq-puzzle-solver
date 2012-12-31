6731f0ef6b99fe59eb285fe8d16e7300d03b13a0
fukayatsu <fukayatsu@gmail.com>
2012-12-31 09:41:03 +0900

    add pending spec

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -1,1 +1,20 @@
 require 'block'
+
+describe Block do
+  context 'new block' do
+    describe 'type A' do
+      it 'volume is 16'
+      it 'has 3 patterns'
+    end
+
+    describe 'type B' do
+      it 'volume is 8'
+      it 'has 6 patterns'
+    end
+
+    describe 'type C' do
+      it 'volume is 1'
+      it 'has 1 pattern'
+    end
+  end
+end
