60a26c6606a01d53d38c8822b907844d5a978d87
fukayatsu <fukayatsu@gmail.com>
2012-12-31 20:16:49 +0900

    [from now] 2012/12/31 20:16:49

--- lib/solver.rb
+++ lib/solver.rb
@@ -5,7 +5,6 @@ class Solver
   attr_reader :step_count, :box, :box_stock
   def initialize
     @step_count = 0
-    @block_stock = Array.new(6, :a) + Array.new(6, :b) + Array.new(5, :c)
   end
 
   def step_count
@@ -15,9 +14,7 @@ class Solver
   def solve(box = nil, block_stack = nil)
     if box
       if box.full?
-        p box
-        'finish'
-        exit
+        return box
       elsif block_stack.size == 0
         return nil
       end
@@ -26,17 +23,19 @@ class Solver
       block_stack = Array.new(6, :a) + Array.new(6, :b) + Array.new(5, :c)
     end
 
+    p block_stack
     blocks = Block.new(block_stack.shift)
     blocks.to_a.each do |block|
       points =  box.points_to_put(block)
       points.each do |point|
         updated_box = Box.new(box.raw_array)
         updated_box.put(point, block, blocks.type.to_s)
-        # puts "**************"
-        p updated_box
 
-        answer = solve(updated_box, block_stack)
-        return answer if answer
+        # puts "**************"
+        # p updated_box
+
+        next_step = solve(updated_box, block_stack)
+        return next_step if next_step
       end
     end
     nil
@@ -45,5 +44,5 @@ end
 
 if __FILE__ == $PROGRAM_NAME
   solver = Solver.new
-  solver.solve
+  p solver.solve
 end
