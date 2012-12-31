102767db19ddcfe88cd6b51ef076ad5b45cc43ef
fukayatsu <fukayatsu@gmail.com>
2012-12-31 16:30:53 +0900

    [from now] 2012/12/31 16:30:53

--- lib/solver.rb
+++ lib/solver.rb
@@ -2,21 +2,29 @@ require_relative 'block'
 require_relative 'box'
 
 class Solver
-  def initialize
-    @step_count = 0
-    @box = Box.new
+  attr_reader :step_count, :box, :box_stock
+  def initialize(solver = nil)
+    if solver
+      @step_count = solver.step_count
+      @box = solver.box
+      @block_stock = solver.block_stock
+    else
+      @step_count = 0
+      @box = Box.new
+      @block_stock = Array.new(6, :a) + Array.new(6, :b) + Array.new(5, :c)
+    end
   end
 
   def step_count
     @step_count
   end
 
-  def step
+  def step(status, pos, block)
     @step_count += 1
   end
 
   def solved?
-    false
+    @box.full?
   end
 end
 
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,5 +1,8 @@
 require 'solver'
 
+require 'box'
+require 'block'
+
 describe Solver do
   context '#new' do
     subject { Solver.new }
@@ -10,7 +13,7 @@ describe Solver do
   context 'first step' do
     before do
       @solver = Solver.new
-      @solver.step
+      @solver.step(nil, [0, 0, 0], Block.new(:c, 0))
     end
     subject { @solver }
     its(:step_count) { should == 1 }
