3859ed76cd76f6848e1f4c814a232cd83068dbf2
fukayatsu <fukayatsu@gmail.com>
2012-12-31 13:00:16 +0900

    [from now] 2012/12/31 13:00:16

--- lib/solver.rb
+++ lib/solver.rb
@@ -1,5 +1,13 @@
 class Solver
+  def initialize
+    @step_count = 0
+  end
+
   def step_count
-    0
+    @step_count
+  end
+
+  def step
+    @step_count += 1
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,4 +5,13 @@ describe Solver do
     subject { Solver.new }
     its(:step_count) { should == 0 }
   end
+
+  context 'first step' do
+    before do
+      @solver = Solver.new
+      @solver.step
+    end
+    subject { @solver }
+    its(:step_count) { should == 1 }
+  end
 end
