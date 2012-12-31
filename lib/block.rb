478439d920f8da4ac80dda6b36e2e45deee416db
fukayatsu <fukayatsu@gmail.com>
2012-12-31 14:12:31 +0900

    [from now] 2012/12/31 14:12:31

--- lib/block.rb
+++ lib/block.rb
@@ -1,12 +1,12 @@
 BLOCK_DATA = {
-  a: [[2, 2, 3], [2, 3, 2], [3, 2, 2]],
-  b: [[1, 2, 4], [1, 4, 2], [2, 1, 4], [2, 4, 1], [4, 1, 2], [4, 2, 1]],
-  c: [[1, 1, 1]]
+  a: { count: 6, data: [[2, 2, 3], [2, 3, 2], [3, 2, 2]] },
+  b: { count: 6, data: [[1, 2, 4], [1, 4, 2], [2, 1, 4], [2, 4, 1], [4, 1, 2], [4, 2, 1]] },
+  c: { count: 5, data: [[1, 1, 1]] }
 }
 
 class Block
   def initialize(type, pattern_number)
-    @size_spec = BLOCK_DATA[type][pattern_number]
+    @size_spec = BLOCK_DATA[type][:data][pattern_number]
   end
 
   def size_spec
