c91d0d1d62e6311fb40ef164d4128587e2fe76dd
fukayatsu <fukayatsu@gmail.com>
2012-12-31 09:44:30 +0900

    [from now] 2012/12/31 09:44:30

--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -3,16 +3,22 @@ require 'block'
 describe Block do
   context 'new block' do
     describe 'type A' do
-      it 'volume is 16'
+      subject {Block.new(:a, 0)}
+
+      it {volume.should == 16}
       it 'has 3 patterns'
     end
 
     describe 'type B' do
+      subject {Block.new(:b, 0)}
+
       it 'volume is 8'
       it 'has 6 patterns'
     end
 
     describe 'type C' do
+      subject {Block.new(:c, 0)}
+
       it 'volume is 1'
       it 'has 1 pattern'
     end
