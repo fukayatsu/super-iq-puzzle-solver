78322a04b4345deb09efd500fb4a6d27141a97c2
fukayatsu <fukayatsu@gmail.com>
2012-12-31 10:11:32 +0900

    remove unneeded specs

--- lib/block.rb
+++ lib/block.rb
@@ -6,15 +6,10 @@ BLOCK_DATA = {
 
 class Block
   def initialize(type, pattern_number)
-    @block_array = BLOCK_DATA[type]
-    @pattern_number = pattern_number
+    @block_array = BLOCK_DATA[type][pattern_number]
   end
 
   def volume
-    @block_array[@pattern_number].inject(1, :*)
-  end
-
-  def pattern_count
-    @block_array.size
+    @block_array.inject(1, :*)
   end
 end
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -5,19 +5,16 @@ describe Block do
     describe 'type A' do
       subject { Block.new(:a, 0) }
       its(:volume)        { should == 16 }
-      its(:pattern_count) { should == 3 }
     end
 
     describe 'type B' do
       subject {Block.new(:b, 0)}
       its(:volume)        { should == 8 }
-      its(:pattern_count) { should == 6 }
     end
 
     describe 'type C' do
       subject {Block.new(:c, 0)}
       its(:volume)        { should == 1 }
-      its(:pattern_count) { should == 1 }
     end
   end
 end
