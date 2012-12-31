773975eada96fea3c0a35d9eebecb3f3b082f1ae
fukayatsu <fukayatsu@gmail.com>
2012-12-31 10:07:52 +0900

    ブロックの定義と読み込み

--- lib/block.rb
+++ lib/block.rb
@@ -1,12 +1,33 @@
+BLOCK_DATA = {
+  a: [
+    [2, 2, 4],
+    [2, 4, 2],
+    [4, 2, 2]
+  ],
+  b: [
+    [1, 2, 4],
+    [1, 4, 2],
+    [2, 1, 4],
+    [2, 4, 1],
+    [4, 1, 2],
+    [4, 2, 1]
+  ],
+  c: [
+    [1, 1, 1]
+  ]
+}
+
 class Block
   def initialize(type, pattern_number)
+    @block_array = BLOCK_DATA[type]
+    @pattern_number = pattern_number
   end
 
   def volume
-    16
+    @block_array[@pattern_number].inject(1, :*)
   end
 
   def pattern_count
-    3
+    @block_array.size
   end
 end
